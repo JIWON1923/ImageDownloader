654d929fc388a7679afdd04e826695773bb726ca
JIWON1923 <zest1923@gmail.com>
2023-02-27 15:40:35 +0900

    [Feat] #2 - 이미지 Load 기능 구현

--- ImageDownloader/Controllers/ViewController.swift
+++ ImageDownloader/Controllers/ViewController.swift
@@ -12,8 +12,21 @@ class ViewController: UIViewController {
     @IBOutlet var webImageViews: [UIImageView]!
     @IBOutlet var loadButtons: [UIButton]!
     
+    private let webImageURL = [
+    "https://user-images.githubusercontent.com/68676844/221489275-d8fd112c-56cc-45f6-926e-418ec907619c.jpg",
+    "https://user-images.githubusercontent.com/68676844/221489336-8606866a-2aeb-497c-a4c0-5e5c6f0e00d8.jpg",
+    "https://user-images.githubusercontent.com/68676844/221489406-e4ef9ab4-c9fa-4d49-8a89-3f446227e73f.jpg",
+    "https://user-images.githubusercontent.com/68676844/221489500-bcac2c01-d4ff-4162-a1bf-e02ab3ec36d8.jpg",
+    "https://user-images.githubusercontent.com/68676844/221489614-5d29abba-80fb-484b-971d-962beee35239.jpg"
+    ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        webImageViews.forEach {
+            $0.layer.masksToBounds = true
+            $0.contentMode = .scaleAspectFill
+        }
     }
     
     
@@ -21,7 +34,7 @@ class ViewController: UIViewController {
         
         for index in loadButtons.indices {
             if loadButtons[index] == sender {
-                print("\(index) tapped!")
+                webImageViews[index].loadImage(urlString: webImageURL[index])
             }
         }
         
@@ -29,8 +42,9 @@ class ViewController: UIViewController {
     
     
     @IBAction func didTappedReloadButton(_ sender: UIButton) {
-        print("did tapped LOAD AL IMAGES !!")
-        
+        zip(webImageViews, webImageURL).forEach { image, url in
+            image.loadImage(urlString: url)
+        }
     }
 }
 
